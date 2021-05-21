e63f43b2742baf19098013efa249ce7621e32aaa
Mbarov <prom@tgv73.ru>
2021-05-21 19:52:13 +0500

    '1'

---
+++ simbirsoft_test/services.py
@@ -0,0 +1,8 @@
+from .dto import ChoiceDTO, QuestionDTO, QuizDTO, AnswerDTO, AnswersDTO
+from typing import List
+
+
+class QuizResultService():
+    def __init__(self, quiz_dto: QuizDTO, answers_dto: AnswersDTO):
+        self.quiz_dto = quiz_dto
+        self.answers_dto = answers_dto
